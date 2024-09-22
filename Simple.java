2803a0517017aed11c11685690184030bd31fbdc
bala <bala20081999@gmail.com>
2024-09-22 11:54:24 +0530

    added umayal

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Bala/sangeetha");  
+     System.out.println("Hello Bala/sangeetha/umayal");  
     }  
 } 
